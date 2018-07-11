3a791cd57bdd842139b16955ae43d46a8036bd09
ks.choi <ks.choi@alticast.com>
2018-07-11 13:32:51 +0900

    update configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var config = {
-    // release시에는 보안으로 인해 source-map으로 해야 함
-    devtool: 'eval-source-map',
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',// release시에는 보안으로 인해 source-map으로 해야 함
     entry: [
         __dirname + '/app/index.js'
     ],
@@ -19,22 +21,23 @@ var config = {
         }, {
             test: /\.css$/,
             use: [
-                {loader: 'style-loader', options: {hmr: true, sourceMap: true}},// style-loader(html에 style추가)
-                {loader: 'css-loader', options: {modules: true, sourceMap: true}}// css-loader(javascript내 import이용하여 css로드하여 class 사용)
+                {loader: 'style-loader'},// style-loader(html에 style추가)
+                {loader: 'css-loader', options: {modules: true}}// css-loader(javascript내 import이용하여 css로드하여 class 사용)
             ]
         }]
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([{from: 'app/data', to: 'data'}]),
+        new webpack.DefinePlugin({"process.env.NODE_ENV": JSON.stringify("development")}),
         new webpack.HotModuleReplacementPlugin(),
-        // 기본 index.html template 지정, template loader(jade, ejs등)를 이용하여 html에 template적용 가능
-        new HtmlWebpackPlugin({
-            template: __dirname + '/app/index.html'
-        })
+        new HtmlWebpackPlugin({template: __dirname + '/app/index.html', cache: true})// 기본 index.html template 지정, template loader(jade, ejs등)를 이용하여 html에 template적용 가능
     ],
     devServer: {
         contentBase: __dirname + '/app',
         port: 80,
         historyApiFallback: true,
+        https: false,
         inline: true,
         hot: true
     }
---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+var config = {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: [
+        __dirname + '/app/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/,
+            use: [
+                {loader: 'style-loader'},
+                {loader: 'css-loader', options: {modules: true}}
+            ]
+        }]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new CopyWebpackPlugin([{from: 'app/data', to: 'data'}]),
+        new UglifyJsPlugin(),
+        new webpack.DefinePlugin({"process.env.NODE_ENV": JSON.stringify("production")}),
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({template: __dirname + '/app/index.html', cache: true})
+    ]
+};
+
+module.exports = config;
